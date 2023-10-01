a931fb5a3bec28b14b1490784b021b085be34183
Akhdanre <danakhdan12@gmail.com>
2023-10-01 21:30:46 +0700

    adjusment on slider window and controller logic

--- view/slider_window.py
+++ view/slider_window.py
@@ -4,7 +4,6 @@ from PyQt5 import QtCore, QtWidgets
 class SliderWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(SliderWindow, self).__init__(parent)
-
         self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
         self.button = QtWidgets.QPushButton("Ok")
 
@@ -17,3 +16,11 @@ class SliderWindow(QtWidgets.QMainWindow):
 
         self.setCentralWidget(central_widget)
         self.setContentsMargins(20, 20, 20, 20)
+
+        desktop = QtWidgets.QApplication.desktop()
+        screen_rect = desktop.screenGeometry()
+        screen_width, screen_height = screen_rect.width(), screen_rect.height()
+
+        window_width, window_height = 200, self.height()
+        self.setGeometry((screen_width - window_width) // 2, (screen_height - window_height) // 2, window_width, window_height)
+
