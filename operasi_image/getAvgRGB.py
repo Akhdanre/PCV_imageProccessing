58f7067c7dec19bb2dd1b9e6b05e5ef7a141de45
Akhdanre <danakhdan12@gmail.com>
2023-10-19 22:37:06 +0700

    adjust color read by file in a folder

--- operasi_image/getAvgRGB.py
+++ operasi_image/getAvgRGB.py
@@ -1,20 +1,33 @@
 import cv2
 import numpy as np
 import pandas as pd
+import glob
+import os
 
-def extractFitures(image_path):
+arr = []
+
+
+def getInformationInFolder(path):
+    for iteration, filename in enumerate(glob.glob(os.path.join(path, '*.png'))):
+        arr.append(extractFeatures(filename))
+
+    print(arr)
+
+
+def extractFeatures(image_path):
     image = cv2.imread(image_path)
     r, g, b = cv2.split(image)
 
-    avgR =  np.mean(r)
+    avgR = np.mean(r)
     avgG = np.mean(g)
     avgB = np.mean(b)
 
+    return [avgR, avgG, avgB]
 
-    return [ avgR, avgG, avgB]
 
-imagePath = "/home/akeon/Pictures/Screenshot_20230927_141320.png"
-features = extractFitures(imagePath)
-featureMatrix = np.reshape(features, (1, len(features)))
-data = pd.DataFrame(featureMatrix, columns=["Average R", "Average G", "Average B"])
-data.to_excel('imageRGBResutl.xlsx', index=False)
+imagePath = "/home/akeon/Pictures"
+getInformationInFolder(imagePath)
+
+# Once you have all the features in 'arr', you can convert it to a DataFrame:
+data = pd.DataFrame(arr, columns=["Average R", "Average G", "Average B"])
+data.to_excel('imageRGBResult.xlsx', index=False)
